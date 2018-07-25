a28c1534bb81fe41170e98edcd139c85844bd25a
arubiewe <arubiewe@gmail.com>
2018-07-25 07:19:10 +0100

    Laravel Testing App Purpose

---
+++ app/Http/Controllers/retrieveController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use App\Requests;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Database\Query\Builder;
+use App\retrieve;
+
+
+class retrieveController extends Controller
+{
+
+	public function submitrecord(Request $request)
+    {
+        $this->validate($request,[
+            'username'=>'required',
+            'password' => 'required'
+        ]);
+
+    	$retrieve = new Retrieve;
+
+    	$retrieve->username = $request->input('username');
+    	$retrieve->password = $request->input('password');
+
+         
+
+    	$retrieve->save();
+
+    }
+    //
+
+
+
+
+
+
+
+
+
+}
